4ed483d97a67586bda4f93bf308b77e317ceba02
Paul <pvsoriano@me.com>
2017-10-01 13:57:40 -0500

    File system hash main program

---
+++ file-sys-hash.py
@@ -0,0 +1,12 @@
+#File System Hashing Program
+#Version 1.0
+
+import logging
+import time
+import sys
+import _file-sys-hash
+
+if __name__ == '__main__':
+	#Turn on logging
+logging.basicConfig(filename='file-sys-hash.log',level=logging.DEBUG,format='%(asctime)s%(message)s')
+
